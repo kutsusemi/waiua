8f5eb022508ac36d2d84660d845f43144c331ec3
kutsusemi <kutsusemi@gmail.com>
2024-11-03 23:08:59 +0900

    initial commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,25 @@
+use tauri::Manager;
+
+// mod commands;
+// mod domain;
+// mod infra;
+// mod module;
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    // let container = module::AppModule::builder().build();
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            // app.manage(container);
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
